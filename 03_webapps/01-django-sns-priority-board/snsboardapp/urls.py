fbf960801b82d2a6712432fd7c99af84d54ea93c
tech-mw <tech-mw@users.noreply.github.com>
2025-08-05 10:11:08 +0900

    login画面、一覧表示画面（一旦表示まで）作成

--- 03_webapps/01-django-sns-priority-board/snsboardapp/urls.py
+++ 03_webapps/01-django-sns-priority-board/snsboardapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . views import signupfunc
+from . views import signup_func, login_func, board_func
 
 urlpatterns = [
-    path('signup/', signupfunc, name='signup'),
+    path('signup/', signup_func, name='signup'),
+    path('login/', login_func, name='login'),
+    path('board/', board_func, name='board'),
 ]
--- 03_webapps/01-django-sns-priority-board/snsboardapp/views.py
+++ 03_webapps/01-django-sns-priority-board/snsboardapp/views.py
@@ -1,8 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.shortcuts import render, redirect
 
 
-def signupfunc(request):
+def signup_func(request):
     """
-    Render the signup page.
+    新規登録
     """
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            User.objects.create_user(username=username, password=password)
+            return render(request, 'signup.html', {'success': '新規登録に成功しました。'})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは既に登録されています'})
     return render(request, 'signup.html', {})
+
+
+def login_func(request):
+    """
+    ログイン
+    """
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('board')
+        else:
+            return render(request, 'login.html', {'context': 'ログインに失敗しました。ユーザー名またはパスワードが正しくありません。'})
+    return render(request, 'login.html', {})
+
+
+def board_func(request):
+    """
+    投稿の一覧表示
+    """
+    # 一覧表示
+    return render(request, 'board.html', {})
